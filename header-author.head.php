75ce1d1b6ae26820d60ac6b5f21139d829444c5c
HeshamAdel0 <heshamadel964@gmail.com>
2019-09-12 21:06:46 +0200

    Fix Style

--- header-author.head.php
+++ header-author.head.php
@@ -30,11 +30,11 @@
     </head>
     <!-- Start Body -->
     <body <?php body_class(); ?>>
-        <nav class="navbar navbar-expand-lg second-nav">
+        <nav class="navbar navbar-expand-lg">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse author-nav" id="navbarNav">
                 <?php foucs_nav_custom_theme_menu() ?>
             </div>
             <?php get_search_form()?>
